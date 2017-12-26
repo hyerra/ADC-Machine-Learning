59435978ea0f5489be9a70c5286d88246e66d010
Harish Yerra <hyerra08@gmail.com>
2017-12-25 18:26:41 -0600

    Add testing code for battery life machine learning.

--- ADC Battery Life Distance Regressor.R
+++ ADC Battery Life Distance Regressor.R
@@ -8,8 +8,11 @@ dataset <- read.csv('Drone Battery Life Data.csv')
 library(caTools)
 split <- sample.split(dataset$Distance.Traveled, SplitRatio = 0.9)
 training_set <- subset(dataset, split == T)
-testing_set <- subset(dataset, split == F)
+test_set <- subset(dataset, split == F)
 
 # Fitting Regression to the Data set
 regressor <- lm(Distance.Traveled..miles. ~ .,
                 data = training_set)
+
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
